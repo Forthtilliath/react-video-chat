4bc8ae8d3f657c8402908416b09139f836adc233
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-12 18:06:08 +0200

    heroku serv

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://fast-forest-19823.herokuapp.com/');
+const socket = io('https://react-video-chat-forth.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
